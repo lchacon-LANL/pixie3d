e68a73a7cfa8bafe1cf8f46219cb7a41d0f27f1c
lchacon <devnull@localhost>
2004-07-09 17:21:40 +0000

    * Merged routine killPreconditioner.f * Eliminated variable allocation (transferred to createNonlinearFunction) * Packed coefficient calculations into a separate routine in module   precond_mod.f * Added code so that diagonals and coefficients are only calculated at the   beginning of the Newton step.

--- src/setupPreconditioner.f
+++ src/setupPreconditioner.f
@@ -26,10 +26,8 @@ c Call variables
 
 c Local variables
 
-      integer(4) :: ntotd2p,order,nxx,nyy,nzz,igrid,alloc_stat
-      real(8)    :: dvol
-
-      real(8), allocatable, dimension(:,:,:,:) :: vector
+cc      integer(4) :: order,nxx,nyy,nzz,igrid
+cc      real(8)    :: dvol
 
 c Debug
 
@@ -43,6 +41,7 @@ c Externals
 
 c Begin program
 
+c diag ******* NUMERICAL GRID QUANTITIES
 cc      open(unit=110,file='debug.bin',form='unformatted'
 cc     .    ,status='replace')
 cc      igx = 1
@@ -51,7 +50,7 @@ cc        do j=1,3
 cc          nx = grid_params%nxv(igx)
 cc          ny = grid_params%nyv(igx)
 cc          nz = grid_params%nzv(igx)
-cc          debug = gmetric%grid(igx)%jac(0:nx+1,0:ny+1,0:nz+1)
+cc          debug = gmetric%grid(igx)%cnv(0:nx+1,0:ny+1,0:nz+1,i,j)
 cccc          debug = gmetric%grid(igx)%Gamma(0:nx+1,0:ny+1,0:nz+1,1,i,j)
 cccc     .           *gmetric%grid(igx)%jac
 cc
@@ -61,6 +60,7 @@ cc        enddo
 cc      enddo
 cc      close(110)
 cc      stop
+c diag *******
 
       if (precon == 'id') return
 
@@ -68,44 +68,11 @@ cc      stop
       igy = 1
       igz = 1
 
-      nxx = grid_params%nxv(igx)
-      nyy = grid_params%nyv(igy)
-      nzz = grid_params%nzv(igz)
+      nx = grid_params%nxv(igx)
+      ny = grid_params%nyv(igy)
+      nz = grid_params%nzv(igz)
 
-      nx = nxx
-      ny = nyy
-      nz = nzz
-
-      ngrid = grid_params%ngrid
-
-      igrid = igx
-
-      ntotd2p = 2*ntot/neqd
-
-      order=0
-
-c Allocate variables
-
-      allocate (mgj0cnv(ntotd2p,3))
-      allocate (mgadvdiffV0(ntotd2p,3))
-      allocate (mgdivV0(ntotd2p))
-      allocate (bcs(6,neqd+3))
-
-      if (jit == 1) then  !Only allocate at the beginning of Newton iteration
-        deallocate (rho_diag,tmp_diag,b_diag,v_diag,STAT=alloc_stat)
-
-        allocate (rho_diag(1,ntotd2p)
-     .           ,tmp_diag(1,ntotd2p)
-     .           ,  b_diag(3,3*ntotd2p)
-     .           ,  v_diag(3,3*ntotd2p),STAT=alloc_stat)
-      endif
-
-      call allocateMGArray(1,gp0)
-      call allocateMGArray(1,grho0)
-      call allocateMGArray(3,gv0)
-      call allocateMGArray(3,gb0)
-
-c Unpack vector x, taking BCs from t=n solution
+c Unpack vector x
 
       varray = x  !This allocates varray; overloaded assignment
 
@@ -125,106 +92,16 @@ c Extract arrays and BC's
       do ieq=1,neqd
         bcs(:,ieq) = varray%array_var(ieq)%bconds(:)
       enddo
+
       !Current boundary conditions
       bcs(:,IJX:IJZ) = bcs(:,IBX:IBZ)
       where (bcs(:,IJX:IJZ) == -NEU)
         bcs(:,IJX:IJZ) = -DIR  !Use covariant components for tangential dirichlet
       end where
 
-c Store density in all grids (w/o BCs)
+c Find coefficients for linearized systems
 
-cc      call restrictArrayToMGVector(1,nxx,nyy,nzz,rho,mgrho0,igrid,order
-cc     .                            ,.false.)
-
-      grho0%grid(igrid)%array(:,:,:,1) = rho
-
-      call restrictMGArray(IRHO,1,grho0,bcs(:,IRHO),igrid,order)
-
-c Store magnetic field components in all grids (w/ BCs)
-
-      gb0%grid(igrid)%array(:,:,:,1) = bx
-      gb0%grid(igrid)%array(:,:,:,2) = by
-      gb0%grid(igrid)%array(:,:,:,3) = bz
-
-      call restrictMGArray(IBX,3,gb0,bcs(:,IBX:IBZ),igrid,order)
-
-c Store ion velocity components in all grids (w/ BCs)
-
-      gv0%grid(igrid)%array(:,:,:,1) = vx
-      gv0%grid(igrid)%array(:,:,:,2) = vy
-      gv0%grid(igrid)%array(:,:,:,3) = vz
-
-      call restrictMGArray(IVX,3,gv0,bcs(:,IVX:IVZ),igrid,order)
-
-c Store current components in all grids (w/o BCs)
-
-      call restrictArrayToMGVector(1,nxx,nyy,nzz,jx,mgj0cnv(:,1),igrid
-     .                  ,order,.false.)
-      call restrictArrayToMGVector(1,nxx,nyy,nzz,jy,mgj0cnv(:,2),igrid
-     .                  ,order,.false.)
-      call restrictArrayToMGVector(1,nxx,nyy,nzz,jz,mgj0cnv(:,3),igrid
-     .                  ,order,.false.)
-
-c Find auxiliary quantities and store them in all grids
-
-      !Velocity divergence (w/o BCs)
-      allocate(divrgV(0:nxx+1,0:nyy+1,0:nzz+1))
-
-      do k=1,nzz
-        do j=1,nyy
-          do i=1,nxx
-            divrgV(i,j,k) = div(i,j,k,nxx,nyy,nzz,vx,vy,vz)
-          enddo
-        enddo
-      enddo
-
-      call restrictArrayToMGVector(1,nxx,nyy,nzz,divrgV,mgdivV0,igrid
-     .                            ,order,.false.)
-
-      deallocate(divrgV)
-
-      !pressure (w/ BCs)
-      gp0%grid(igrid)%array(:,:,:,1) = 2.*rho*tmp
-
-      call restrictMGArray(IRHO,1,gp0,bcs(:,IRHO),igrid,order)
-
-      !v0/dt+theta(v0.grad(v0)-mu*veclap(v0))
-
-      allocate(vector(0:nxx+1,0:nyy+1,0:nzz+1,3))
-
-      do k = 1,nzz
-        do j = 1,nyy
-          do i = 1,nxx
-            ii  = i + nxx*(j-1) + nxx*nyy*(k-1)
-
-            jac    = gmetric%grid(igx)%jac(i,j,k)
-            nabla_v= fnabla_v(i,j,k,nxx,nyy,nzz,vx,vy,vz,0)
-            dvol   = volume(i,j,k,igx,igy,igz)
-
-            !Eqn 1
-            do icomp=1,3
-              vector(i,j,k,icomp) = 
-     .                            gv0%grid(igrid)%array(i,j,k,icomp)/dt
-     .                          + alpha*vx(i,j,k)*nabla_v(1,icomp)/jac
-     .                          + alpha*vy(i,j,k)*nabla_v(2,icomp)/jac
-     .                          + alpha*vz(i,j,k)*nabla_v(3,icomp)/jac
-     .                          - alpha*veclaplacian(i,j,k,nxx,nyy,nzz
-     .                                              ,vx,vy,vz,nuu
-     .                                              ,alt_eom,icomp)/dvol
-            enddo
-
-          enddo
-        enddo
-      enddo
-
-      do icomp=1,3
-        call restrictArrayToMGVector(1,nxx,nyy,nzz
-     .                              ,vector(:,:,:,icomp)
-     .                              ,mgadvdiffV0(:,icomp)
-     .                              ,igrid,order,.false.)
-      enddo
-
-      deallocate(vector)
+      if (jit == 1) call findCoeffs
 
 c Find required diagonals in all grids
 
@@ -240,10 +117,11 @@ c Find required diagonals in all grids
 
         icomp = IVX
         call find_mf_diag_std(3,3*ntotdp,v_mtvc,1,bcs(:,IVX:IVZ),v_diag)
+
+        form_diag = .false.
       endif
 
-c diag
-c DIAGONAL GENERATION FOR XDRAW PLOTTING
+c diag ***** DIAGONAL GENERATION FOR XDRAW PLOTTING
 cc      do k=1,nzz
 cc        do j=1,nyy
 cc          do i=1,nxx
@@ -258,9 +136,9 @@ cc     .    ,status='replace')
 cc      call contour(debug(1:nxx,1:nyy,1),nxx,nyy,0d0,xmax,0d0,ymax,0,110)
 cc      close(110)
 cc      stop
+c diag *****
 
-c diag ***************
-c MATRIX GENERATION FOR MATLAB PROCESSING
+c diag ***** MATRIX GENERATION FOR MATLAB PROCESSING
 cc      igrid = 1
 cc
 cc      nxx = grid_params%nxv(igrid)
@@ -285,7 +163,22 @@ cc        write (110,*) (v_mat(i,j),j=1,nt)
 cc      enddo
 cc      close(110)
 cc      stop
+c diag *****
 
 c End program
 
       end subroutine setupPreconditioner
+
+c killPreconditioner
+c###################################################################
+      subroutine killPreconditioner
+
+      use precond_variables
+
+c     Deallocate variables
+
+      if (precon == 'id') return
+
+cc      call deallocPrecVariables
+
+      end subroutine killPreconditioner
