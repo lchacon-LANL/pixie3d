d44bc3faebdfed9b18629ea4c3ebfa90895df03b
lchacon <devnull@localhost>
2005-01-07 18:56:12 +0000

    * Added possibility of non-div-free ZIP discretization in Faraday's law. * Changed code to use generalized code of div(vB-Bv).

--- src/nonlinearRHS.f
+++ src/nonlinearRHS.f
@@ -45,8 +45,6 @@ c Local variables
       real(8)    :: Ez_jp,Ez_jm,Ey_kp,Ey_km,Ex_kp,Ex_km
      .             ,Ez_ip,Ez_im,Ey_ip,Ey_im,Ex_jp,Ex_jm
 
-      logical    :: alt_eom_b
-
       ! EOM
       real(8)    :: t11p,t12p,t13p,t11m,t12m,t13m,t11o,t12o,t13o
      .             ,t21p,t22p,t23p,t21m,t22m,t23m,t21o,t22o,t23o
@@ -185,172 +183,21 @@ cc      endif
 
 c     Faraday's law
 
-      alt_eom_b = .false.
+      vec1 => vcnv
+      vec2 => bcnv
 
-      ff(IBX:IBZ)= div_tensor(i,j,k,nx,ny,nz,igx,igy,igz,alt_eom_b
-     .                       ,btensor_x,btensor_y,btensor_z)
+      if (.not.zip_eom_b) then
+        ff(IBX:IBZ)= div_tensor(i,j,k,nx,ny,nz,igx,igy,igz,.false.
+     .                         ,btensor_x,btensor_y,btensor_z)
      .           + jac*dvol*curl(i,j,k,nx,ny,nz,igx,igy,igz,ejx,ejy,ejz)
-cc     $           - eeta(i,j,k)*veclaplacian(i,j,k,nx,ny,nz,igx,igy,igz
-cc     $                                     ,bcnv,alt_eom_b)
+      else
+        ff(IBX:IBZ)= div_tensor(i,j,k,nx,ny,nz,igx,igy,igz,.false.
+     .                         ,btensor_x,btensor_y,btensor_z)
+     .             - eeta(i,j,k)*veclaplacian(i,j,k,nx,ny,nz,igx,igy,igz
+     .                                       ,bcnv,.false.)
+      endif
 
-c     Bx
-
-cc      flxip = 0d0
-cc      flxim = 0d0
-cc
-cc      flxjp = 0.5*(vy(i,jp,k)*bx(i,jp,k)/jacjp
-cc     .           + vy(i,j ,k)*bx(i,j ,k)/jac  )
-cc     .       -0.5*(vx(i,jp,k)*by(i,jp,k)/jacjp
-cc     .           + vx(i,j ,k)*by(i,j ,k)/jac  )
-cc      flxjm = 0.5*(vy(i,jm,k)*bx(i,jm,k)/jacjm
-cc     .           + vy(i,j ,k)*bx(i,j ,k)/jac  )
-cc     .       -0.5*(vx(i,jm,k)*by(i,jm,k)/jacjm
-cc     .           + vx(i,j ,k)*by(i,j ,k)/jac  )
-cc      Ez_jp = 0.5*(eeta(i,jp,k)*jz_cov(i,jp,k)
-cc     .           + eeta(i,j ,k)*jz_cov(i,j ,k))
-cc      Ez_jm = 0.5*(eeta(i,jm,k)*jz_cov(i,jm,k)
-cc     .           + eeta(i,j ,k)*jz_cov(i,j ,k))
-cc
-cc      flxkp = 0.5*(vz(i,j,kp)*bx(i,j,kp)/jackp
-cc     .           + vz(i,j,k )*bx(i,j,k )/jac  )
-cc     .       -0.5*(vx(i,j,kp)*bz(i,j,kp)/jackp
-cc     .           + vx(i,j,k )*bz(i,j,k )/jac  )
-cc      flxkm = 0.5*(vz(i,j,km)*bx(i,j,km)/jackm
-cc     .           + vz(i,j,k )*bx(i,j,k )/jac  )
-cc     .       -0.5*(vx(i,j,km)*bz(i,j,km)/jackm
-cc     .           + vx(i,j,k )*bz(i,j,k )/jac  )
-cc      Ey_kp = 0.5*(eeta(i,j,kp)*jy_cov(i,j,kp)
-cc     .           + eeta(i,j,k )*jy_cov(i,j,k ))
-cc      Ey_km = 0.5*(eeta(i,j,km)*jy_cov(i,j,km)
-cc     .           + eeta(i,j,k )*jy_cov(i,j,k ))
-cc
-cc      !Add resistive contribution
-cc      flxjp = flxjp + Ez_jp
-cc      flxjm = flxjm + Ez_jm
-cc      flxkp = flxkp - Ey_kp
-cc      flxkm = flxkm - Ey_km
-cc
-cc      !Group fluxes
-cc      ff(IBX) = jac*( dS1*(flxip-flxim)
-cc     .               +dS2*(flxjp-flxjm)
-cc     .               +dS3*(flxkp-flxkm) )
-cc
-ccc     By
-cc
-cc      flxjp = 0d0
-cc      flxjm = 0d0
-cc
-cc      !Ideal contribution
-cc      flxip = 0.5*(vx(ip,j,k)*by(ip,j,k)/jacip
-cc     .            +vx(i ,j,k)*by(i ,j,k)/jac  )
-cc     .       -0.5*(vy(ip,j,k)*bx(ip,j,k)/jacip
-cc     .            +vy(i ,j,k)*bx(i ,j,k)/jac  )
-cc      Ez_ip = 0.5*(eeta(ip,j,k)*jz_cov(ip,j,k)
-cc     .           + eeta(i ,j,k)*jz_cov(i ,j,k))
-cc      if (.not.sing_point) then
-cc        flxim = 0.5*(vx(im,j,k)*by(im,j,k)/jacim
-cc     .             + vx(i ,j,k)*by(i ,j,k)/jac  )
-cc     .         -0.5*(vy(im,j,k)*bx(im,j,k)/jacim
-cc     .             + vy(i ,j,k)*bx(i ,j,k)/jac  )
-cc        Ez_im = 0.5*(eeta(im,j,k)*jz_cov(im,j,k)
-cc     .             + eeta(i ,j,k)*jz_cov(i ,j,k))
-cc      else
-cc        flxim = vx(im,j,k)*by(im,j,k)/jacim
-cc     .         -vy(im,j,k)*bx(im,j,k)/jacim
-cc        Ez_im = eeta(im,j,k)*jz_cov(im,j,k)
-cc      endif
-cc
-cc
-cc      flxkp = 0.5*(vz(i,j,kp)*by(i,j,kp)/jackp
-cc     .           + vz(i,j,k )*by(i,j,k )/jac  )
-cc     .       -0.5*(vy(i,j,kp)*bz(i,j,kp)/jackp
-cc     .           + vy(i,j,k )*bz(i,j,k )/jac  )
-cc      flxkm = 0.5*(vz(i,j,km)*by(i,j,km)/jackm
-cc     .           + vz(i,j,k )*by(i,j,k )/jac  )
-cc     .       -0.5*(vy(i,j,km)*bz(i,j,km)/jackm
-cc     .           + vy(i,j,k )*bz(i,j,k )/jac)
-cc      Ex_kp = 0.5*(eeta(i,j,kp)*jx_cov(i,j,kp)
-cc     .           + eeta(i,j,k )*jx_cov(i,j,k ))
-cc      Ex_km = 0.5*(eeta(i,j,km)*jx_cov(i,j,km)
-cc     .           + eeta(i,j,k )*jx_cov(i,j,k ))
-cc
-cc      !Add resistive contribution
-cc      flxip = flxip - Ez_ip
-cc      flxim = flxim - Ez_im
-cc
-cc      flxkp = flxkp + Ex_kp
-cc      flxkm = flxkm + Ex_km
-cc
-cc      !Group fluxes
-cc      ff(IBY) = jac*( dS1*(flxip-flxim)
-cc     .               +dS2*(flxjp-flxjm)
-cc     .               +dS3*(flxkp-flxkm) )
-cc
-ccc     Bz
-cc
-cc      flxkp = 0d0
-cc      flxkm = 0d0
-cc
-cc      if (sing_point) then
-cc        jach = 0.5*(jac+jacip)
-cc
-cc        flxip = 0.5*(vx(ip,j,k)*bz(ip,j,k)/jacip**2
-cc     .             + vx(i ,j,k)*bz(i ,j,k)/jac**2  )*jach
-cc     .         -0.5*(vz(ip,j,k)*bx(ip,j,k)/jacip**2
-cc     .             + vz(i ,j,k)*bx(i ,j,k)/jac**2  )*jach
-cccc        flxip = 0.5*(vx(ip,j,k)*bz(ip,j,k)/jacip
-cccc     .             + vx(i ,j,k)*bz(i ,j,k)/jac  )
-cccc     .         -0.5*(vz(ip,j,k)*bx(ip,j,k)/jacip
-cccc     .             + vz(i ,j,k)*bx(i ,j,k)/jac  )
-cc        Ey_ip = 0.5*(eeta(ip,j,k)*jy_cov(ip,j,k)/jacip
-cc     .             + eeta(i ,j,k)*jy_cov(i ,j,k)/jac  )*jach
-cc
-cccc        flxim = 0d0 !SP BC on ideal flux (since flx_x ~ r)
-cc        flxim = vx(im,j,k)*bz(im,j,k)/jacim
-cc     .         -vz(im,j,k)*bx(im,j,k)/jacim
-cc        Ey_im = eeta(im,j,k)*jy_cov(im,j,k) !Current at SP
-cc      else
-cc        flxip = 0.5*(vx(ip,j,k)*bz(ip,j,k)/jacip
-cc     .             + vx(i ,j,k)*bz(i ,j,k)/jac  )
-cc     .         -0.5*(vz(ip,j,k)*bx(ip,j,k)/jacip
-cc     .             + vz(i ,j,k)*bx(i ,j,k)/jac  )
-cc        flxim = 0.5*(vx(im,j,k)*bz(im,j,k)/jacim
-cc     .             + vx(i ,j,k)*bz(i ,j,k)/jac  )
-cc     .         -0.5*(vz(im,j,k)*bx(im,j,k)/jacim
-cc     .             + vz(i ,j,k)*bx(i ,j,k)/jac  )
-cc
-cc        Ey_ip = 0.5*(eeta(ip,j,k)*jy_cov(ip,j,k)
-cc     .             + eeta(i ,j,k)*jy_cov(i ,j,k))
-cc        Ey_im = 0.5*(eeta(im,j,k)*jy_cov(im,j,k)
-cc     .             + eeta(i ,j,k)*jy_cov(i ,j,k))
-cc      endif
-cc
-cc
-cc      flxjp = 0.5*(vy(i,jp,k)*bz(i,jp,k)/jacjp
-cc     .           + vy(i,j ,k)*bz(i,j ,k)/jac  )
-cc     .       -0.5*(vz(i,jp,k)*by(i,jp,k)/jacjp
-cc     .           + vz(i,j ,k)*by(i,j ,k)/jac  )
-cc      flxjm = 0.5*(vy(i,jm,k)*bz(i,jm,k)/jacjm
-cc     .           + vy(i,j ,k)*bz(i,j ,k)/jac  )
-cc     .       -0.5*(vz(i,jm,k)*by(i,jm,k)/jacjm
-cc     .           + vz(i,j ,k)*by(i,j ,k)/jac  )
-cc
-cc      Ex_jp = 0.5*(eeta(i,jp,k)*jx_cov(i,jp,k)
-cc     .           + eeta(i,j ,k)*jx_cov(i,j ,k))
-cc      Ex_jm = 0.5*(eeta(i,jm,k)*jx_cov(i,jm,k)
-cc     .           + eeta(i,j ,k)*jx_cov(i,j ,k))
-cc
-cc      !Add resistive contribution
-cc      flxip = flxip + Ey_ip
-cc      flxim = flxim + Ey_im
-cc
-cc      flxjp = flxjp - Ex_jp
-cc      flxjm = flxjm - Ex_jm
-cc
-cc      !Group fluxes
-cc      ff(IBZ) = jac*( dS1*(flxip-flxim)
-cc     .               +dS2*(flxjp-flxjm)
-cc     .               +dS3*(flxkp-flxkm) )
+      nullify(vec1,vec2)
 
 c     Temperature
 
@@ -431,116 +278,6 @@ c     EOM
 
       ff(IVX:IVZ) = div_tensor(i,j,k,nx,ny,nz,igx,igy,igz,alt_eom
      .                        ,vtensor_x,vtensor_y,vtensor_z)
-
-ccc     Vx
-cc
-cc      call vtensor_x(i ,j,k,igx,igy,igz,t11p,t12p,t13p, 1)
-cc      call vtensor_x(im,j,k,igx,igy,igz,t11m,t12m,t13m,-1)
-cc      if (coords /= 'car')
-cc     .     call vtensor_x(i,j,k,igx,igy,igz,t11o,t12o,t13o, 0)
-cc
-cc      call vtensor_y(i,j ,k,igx,igy,igz,t21p,t22p,t23p, 1)
-cc      call vtensor_y(i,jm,k,igx,igy,igz,t21m,t22m,t23m,-1)
-cc      if (coords /= 'car')
-cc     .     call vtensor_y(i,j,k,igx,igy,igz,t21o,t22o,t23o, 0)
-cc
-cc      call vtensor_z(i,j,k ,igx,igy,igz,t31p,t32p,t33p, 1)
-cc      call vtensor_z(i,j,km,igx,igy,igz,t31m,t32m,t33m,-1)
-cc      if (coords /= 'car')
-cc     .     call vtensor_z(i,j,k,igx,igy,igz,t31o,t32o,t33o, 0)
-cc
-cc      flxip = t11p
-cc      flxim = t11m
-cc
-cc      flxjp = t21p
-cc      flxjm = t21m
-cc
-cc      flxkp = t31p
-cc      flxkm = t31m
-cc
-cc      if (sing_point) flxim = 0d0
-cc
-cc      if (coords == 'car') then
-cc        msource = 0d0
-cc      else
-cc        msource =dvol
-cc     .          *(t11o*hess(1,1,1)+t12o*hess(1,1,2)+t13o*hess(1,1,3)
-cc     .           +t21o*hess(1,2,1)+t22o*hess(1,2,2)+t23o*hess(1,2,3)
-cc     .           +t31o*hess(1,3,1)+t32o*hess(1,3,2)+t33o*hess(1,3,3))
-cc      endif
-cc
-cc      ff(IVX) = jac*( dS1*(flxip - flxim)
-cc     .              + dS2*(flxjp - flxjm)
-cc     .              + dS3*(flxkp - flxkm) ) + msource
-cc
-ccc     Vy
-cc
-cc      flxip = t12p
-cc      flxim = t12m
-cc
-cc      flxjp = t22p
-cc      flxjm = t22m
-cc
-cc      flxkp = t32p
-cc      flxkm = t32m
-cc
-cc      if (coords == 'car') then
-cc        ff(IVY) =  dS1*(flxip - flxim)
-cc     .           + dS2*(flxjp - flxjm)
-cc     .           + dS3*(flxkp - flxkm)
-cc      else
-cc        if (alt_eom) then
-cc
-cc          if (sing_point) flxim = 0d0
-cc
-cc          msource=dvol
-cc     .           *(t11o*hess(2,1,1)+t12o*hess(2,1,2)+t13o*hess(2,1,3)
-cc     .            +t21o*hess(2,2,1)+t22o*hess(2,2,2)+t23o*hess(2,2,3)
-cc     .            +t31o*hess(2,3,1)+t32o*hess(2,3,2)+t33o*hess(2,3,3)
-cc     .            -t12o*(hess(1,1,1)+hess(2,1,2)+hess(3,1,3))
-cc     .            -t22o*(hess(1,2,1)+hess(2,2,2)+hess(3,2,3))
-cc     .            -t32o*(hess(1,3,1)+hess(2,3,2)+hess(3,3,3)))
-cc
-cc          ff(IVY) =   dS1*(flxip - flxim)
-cc     .              + dS2*(flxjp - flxjm)
-cc     .              + dS3*(flxkp - flxkm) + msource
-cc        else
-cc          msource=dvol
-cc     .            *(t11o*hess(2,1,1)+t12o*hess(2,1,2)+t13o*hess(2,1,3)
-cc     .             +t21o*hess(2,2,1)+t22o*hess(2,2,2)+t23o*hess(2,2,3)
-cc     .             +t31o*hess(2,3,1)+t32o*hess(2,3,2)+t33o*hess(2,3,3))
-cc
-cc          ff(IVY) = jac*( dS1*(flxip - flxim)
-cc     .                  + dS2*(flxjp - flxjm)
-cc     .                  + dS3*(flxkp - flxkm) ) + msource
-cc        endif
-cc      endif
-cc
-ccc     Vz
-cc
-cc      flxip = t13p
-cc      flxim = t13m
-cc
-cc      flxjp = t23p
-cc      flxjm = t23m
-cc
-cc      flxkp = t33p
-cc      flxkm = t33m
-cc
-cc      if (sing_point) flxim = 0d0
-cc
-cc      if (coords == 'car') then
-cc        msource = 0d0
-cc      else
-cc        msource =dvol
-cc     .          *(t11o*hess(3,1,1)+t12o*hess(3,1,2)+t13o*hess(3,1,3)
-cc     .           +t21o*hess(3,2,1)+t22o*hess(3,2,2)+t23o*hess(3,2,3)
-cc     .           +t31o*hess(3,3,1)+t32o*hess(3,3,2)+t33o*hess(3,3,3))
-cc      endif
-cc
-cc      ff(IVZ) = jac*( dS1*(flxip - flxim)
-cc     .              + dS2*(flxjp - flxjm)
-cc     .              + dS3*(flxkp - flxkm) ) + msource
 
 c     Divide by cell volume factor
 
